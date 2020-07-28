444dd4af3263ec28035e9a75ed04c892a947aa9b
J Mulder <jlmulder2014@gmail.com>
2020-07-28 11:51:42 -0700

    filterbyID

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 //add route
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -57,6 +62,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 //listen for requests
